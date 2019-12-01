d4a14df1ca1092d887e1e5930827f2a9d0a153aa
Koos du Preez <koos@terraclear.com>
2019-11-30 19:48:08 -0800

    more cleanup

--- install.sh
+++ install.sh
@@ -9,11 +9,11 @@ fi
 echo "setting to /usr/local/bin/fan-daemon/..."
 rm -rf /usr/bin/fan-daemon/ 2>/dev/null
 mkdir /usr/local/bin/fan-daemon
-cp ./bin/Release/fan-daemon /usr/local/bin/fan-daemon/
+cp ./bin/fan-daemon /usr/local/bin/fan-daemon/
 echo "done"
 
 echo "adding service to /lib/systemd/system/..."
-cp fan-daemon.service /lib/systemd/system/
+cp ./svc/fan-daemon.service /lib/systemd/system/
 chmod 644 /lib/systemd/system/fan-daemon.service
 echo "done"
 
